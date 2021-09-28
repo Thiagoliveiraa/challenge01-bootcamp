e984b73f23aad5c087b786cde57a188c498cc822
Thiago Oliveira <thiagooliveiraaa@hotmail.com>
2021-09-27 22:49:59 -0300

    Create Exceptions, insert, update and delete

--- challenge01/src/main/java/com/bootcamp/challenge01/challenge01/services/exceptions/ResourceNotFoundException.java
+++ challenge01/src/main/java/com/bootcamp/challenge01/challenge01/services/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.bootcamp.challenge01.challenge01.services.entities;
+package com.bootcamp.challenge01.challenge01.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
--- challenge01/src/main/resources/data.sql
+++ challenge01/src/main/resources/data.sql
@@ -1,1 +1,16 @@
-INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Mcnutty','123.456.789-01',1567.89,'1987-02-10T10:30:00Z',2);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Tony Soprano','123.456.789-01',100000.00,'1961-09-18T10:30:00Z',2);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Christopher Moltisanti','456.789.012-34',3853.45,'1966-03-26T02:30:00Z',1);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Paulo Gualtieri','005.456.789-01',100000.00,'1961-09-18T10:30:00Z',0);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Silvio Dante','499.789.012-34',3853.45,'1966-03-26T02:30:00Z',3);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Walter White','321.456.789-01',100000.00,'1961-09-18T10:30:00Z',2);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Jesse Pinkman','326.789.012-34',3853.45,'1966-03-26T02:30:00Z',0);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Saul Goodman','120.456.789-01',100000.00,'1961-09-18T10:30:00Z',7);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Thomas Shelby','406.789.012-34',3853.45,'1966-03-26T02:30:00Z',1);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Jax Teller','123.456.789-01',100000.00,'1961-09-18T10:30:00Z',2);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Tara Knowles','456.789.012-34',3853.45,'1966-03-26T02:30:00Z',2);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Joey Tribiane','121.456.789-01',2123.00,'1961-09-18T10:30:00Z',3);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Carmela Soprano','468.013.512-34',3853.45,'1966-03-26T02:30:00Z',2);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Chandler Bing','109.876.543-12',1000.00,'1961-09-18T10:30:00Z',1);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Monica Geller','456.789.012-34',3853.45,'1966-03-26T02:30:00Z',1);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Phoebe Buffay','112.233.445-56',10000.00,'1963-07-30T14:30:00Z',0);
+INSERT INTO tb_client (name,cpf,income,birth_date,children) VALUES ('Rachel Green','445.566.778-89',30853.45,'1969-02-11T12:30:00Z',0);
